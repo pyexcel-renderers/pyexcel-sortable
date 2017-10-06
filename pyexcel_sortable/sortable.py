20c370a1fef821faa7a1817c968cdb93f1ca2c0c
chfw <wangc_2011@hotmail.com>
2017-10-06 08:22:15 +0100

    :sparkles: experimental support for notebook

--- pyexcel_sortable/sortable.py
+++ pyexcel_sortable/sortable.py
@@ -11,12 +11,28 @@ from pyexcel.renderer import Renderer
 from pyexcel._compact import PY2
 
 
-from csvtotable.convert import render_template, freeze_js
+from excel2table.convert import render_template, freeze_js
 
 
 class Sortable(Renderer):
+    def render_sheet_to_stream(self, file_stream, sheet,
+                               write_title=True, caption="",
+                               display_length=None,
+                               **keywords):
+        self.set_write_title(write_title)
+        self.set_output_stream(file_stream)
+
+        if len(sheet.colnames) == 0:
+            sheet.name_columns_by_row(0)
+
+        html = render_template(
+            sheet.colnames, sheet.array[1:],
+            notebook=True, # difference
+            caption=caption, display_length=display_length)
+        self._stream.write(html)
+
     def render_sheet(self, sheet, caption="", display_length=None,
-                     **keywords):
+                             **keywords):
         if len(sheet.colnames) == 0:
             sheet.name_columns_by_row(0)
 
